7ecc4ef1eabfe0c8cd7d0dcae3cd54a6fc297d76
lining4069 <13400164069@163.com>
2026-02-14 22:00:50 +0800

    fix(api):修正用户module下路由前缀

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -7,4 +7,4 @@ api_router = APIRouter()
 
 # tags:z在接口文档中的分组名
 api_router.include_router(news.router, prefix="/news", tags=["News"])
-api_router.include_router(users.router, prefix="/users", tags=["Users"])
+api_router.include_router(users.router, prefix="/user", tags=["Users"])
